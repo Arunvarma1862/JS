d492be8d8d0c054883028bbcabad7e05e70a72d6
arunvarma <arunbabu120894@gmail.com>
2024-11-04 21:22:09 +0530

    4/11

--- src/class10/js109.js
+++ src/class10/js109.js
@@ -4,4 +4,4 @@ const sectionTodo= document.querySelector(".section-todo");
 // const infos=document.body.getBoundingClientRect();
 // const infos=sectionTodo.getBoundingClientRect();
 const infos=sectionTodo.getBoundingClientRect().height;
-console.log(infos)
+console.log(infos);
