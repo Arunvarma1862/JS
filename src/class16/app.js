09ffdc2f0acf428bb82c317a65c488f8d82dc2c9
arunvarma <arunbabu120894@gmail.com>
2024-11-15 12:07:55 +0530

    15/11

--- src/class16/app.js
+++ src/class16/app.js
@@ -1,11 +1,14 @@
 const modal= document.querySelector(".modal");
 const openModalButton= document.querySelector(".openModalButton");
 const closeModal= document.querySelector(".close_button");
+const overlay= document.querySelector(".overlay");
 const openModal=()=>{
-    modal.classList.remove("close")
+    modal.classList.remove("close");
+    document.body.classList.add("overflowHidden")
 }
 const closeModals=()=>{
-    modal.classList.add("close")
+    modal.classList.add("close");
+    document.body.classList.remove("overflowHidden")
 }
 
 openModalButton.addEventListener("click",(e)=>{
@@ -13,7 +16,23 @@ openModalButton.addEventListener("click",(e)=>{
     openModal()
 })
 
-closeModal.addEventListener("click",(e)=>{
-   closeModals()
+// closeModal.addEventListener("click",(e)=>{
+//    closeModals();
+   
+// })
+// overlay.addEventListener("click",()=>{
+//     closeModals();
+// })
+
+modal.addEventListener("click",(e)=>{
+    //  console.log(e.target);
+     const targetClassList= e.target.classList;
+     if(targetClassList.contains("overlay") || targetClassList.contains("close_button")|| targetClassList.contains("modal_button_no")){
+            closeModals()
+     }
+     if(targetClassList.contains("modal_button_yes")){
+        alert("file deleted suceessfully");
+        closeModals()
+     }
 })
 
