05aa494818ecb4a9905cc11829a55f34a5f7f36c
arunvarma <arunbabu120894@gmail.com>
2024-11-20 21:50:53 +0530

    20/11

--- src/class3/js15.js
+++ src/class3/js15.js
@@ -3,10 +3,10 @@
 let fruits=["apple","mango","grapes"];
 
 console.log(fruits)
-// fruits.push("kiwi");// push- add kiwi at last and can return new length in new variable
-// fruits.pop()  // pop- remove item at last and can store removed value in new variable
+// fruits.push("kiwi"); // push- add kiwi at last and can return new length in new variable
+// fruits.pop()        // pop- remove item at last and can store removed value in new variable
 // fruits.shift()   //shift -remove item of first and return removed item can store in new variable
-// fruits.unshift("kiwi") unshift- add item at first and can return new length in new variable 
+// fruits.unshift("kiwi") //unshift- add item at first and can return new length in new variable 
 // console.log(fruits); 
 
 
